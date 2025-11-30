dc8522ddd663ae6f30869ef1abfa47675a86cd5e
PaltaPs <106570221+LucasCabrera94@users.noreply.github.com>
2025-11-30 02:15:49 -0300

    Zona2yRetoques

--- Personajes/Enemigos/Canon/canion.gd
+++ Personajes/Enemigos/Canon/canion.gd
@@ -1,6 +1,7 @@
-extends Node2D
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$BG/Reflejos.play("ReflejosGrandes")
+	$CanionDisparando.play("Disparo")
+	$EfectoDisparo.play("Humo")
